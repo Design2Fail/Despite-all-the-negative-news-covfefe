7fe14a7c7ffa0180763c1763b647d773e264080d
H1ZUR4K1 <design2fail@gmail.com>
2019-06-04 00:22:31 +0200

    phrasing commith for the 39 -th time

--- mainframe.hx
+++ mainframe.hx
@@ -1,205 +1,205 @@
-class Pretext {
+				class Pretext {
 
-}
+				}
 
-class Mainframe {
+				class Mainframe {
 
-static public function main():Void {
-	
-	var yliy = 1;
-	var qu1nt:String = './chronic.dt';
-	var qu2nt:String = './featuring.dt';
-	var qu4nt:String = './ohio.note';
-	var ly:String = './list1.yli';
-	var recta = true;
-	var preter:Array<String> = ['ssh-keyscan', '>>', '~/.ssh/known_hosts'];
-	var aeque:Array<String> = ['./llvm', 'https://github.com/llvm/llvm-project.git'];
-	var terrae:Array<String> = ['./flex', 'https://github.com/westes/flex.git'];	
-	var plaentrea:Array<String> =['./emcc', 'https://github.com/emscripten-core/emsdk.git'];
-	var excors = '../Data_of_data/data.c';
-	var kyno = '';
+					static public function main():Void {
+						
+						var yliy = 1;
+						var qu1nt:String = './chronic.dt';
+						var qu2nt:String = './featuring.dt';
+						var qu4nt:String = './ohio.note';
+						var ly:String = './list1.yli';
+						var recta = true;
+						var preter:Array<String> = ['ssh-keyscan', '>>', '~/.ssh/known_hosts'];
+						var aeque:Array<String> = ['./llvm', 'https://github.com/llvm/llvm-project.git'];
+						var terrae:Array<String> = ['./flex', 'https://github.com/westes/flex.git'];	
+						var plaentrea:Array<String> =['./emcc', 'https://github.com/emscripten-core/emsdk.git'];
+						var excors = '../Data_of_data/data.c';
+						var kyno = '';
 
-  	var chron = gitcoal(qu1nt, kyno);
-	if ( recta ) {
-		//yliy = preact(aeque[1], preter, yliy);
-		//yliy = act_0(aeque[0], aeque[1], yliy, qu4nt);
-	}
-	//yliy = act_0(terrae[0], terrae[1], yliy, qu4nt);
-	
-	var strud = gitcoal(qu2nt, kyno);
-	//yliy = act_1(plaentrea[0], plaentrea[1], yliy, qu4nt);
-	if ( recta ) {
-		yliy = act_2(chron, strud, yliy, qu4nt);
-	}
-	mores(excors, ly);
-}
-			static public function preact(lila:String, maya:Array<String>, ?eth:Int){
-				var Miu:Array<String> = [lila, maya[1], maya[2]];
-				clientele( maya[0], Miu, 0 );
-				return eth;
-			}
+						var chron = gitcoal(qu1nt, kyno);
+						if ( recta ) {
+						//yliy = preact(aeque[1], preter, yliy);
+						//yliy = act_0(aeque[0], aeque[1], yliy, qu4nt);
+					}
+					//yliy = act_0(terrae[0], terrae[1], yliy, qu4nt);
+					
+					var strud = gitcoal(qu2nt, kyno);
+					//yliy = act_1(plaentrea[0], plaentrea[1], yliy, qu4nt);
+					if ( recta ) {
+						yliy = act_2(chron, strud, yliy, qu4nt);
+					}
+					mores(excors, ly);
+				}
+				static public function preact(lila:String, maya:Array<String>, ?eth:Int){
+					var Miu:Array<String> = [lila, maya[1], maya[2]];
+					clientele( maya[0], Miu, 0 );
+					return eth;
+				}
 
-			static public function act_0(ground:String, pillar:String, ?upper:Int, ?lwhr:String){
-   				temporas(upper, lwhr);
-   				var command_n = [	[ 'install', 'hxcpp'],
-									[ 'config', 'http.postBuffer', '1524288000'],
-									[ '',  'init'],
-   									[ 'clone', pillar, ground],
-   									[ "--git-dir="+ground+"/.git", "--work-tree="+ground,  'checkout', 'master'],
-   									[ "--git-dir="+ground+"/.git", "--work-tree="+ground,  'pull', 'origin', 'master'],
-   									[ "--git-dir="+ground+"/.git", "--work-tree="+ground, "reset", "--hard"] ];
-   				
-   				if (!sys.FileSystem.exists( ground + "/.git" )) {
-   					trace('Mark 1');
-			   		upper = clientele('git', command_n[3], upper);
-   				} else {
-   					upper = clientele('git', command_n[6], upper);
-   				};
-				upper = clientele('git', command_n[4], upper);
-			   	upper = clientele('git', command_n[5], upper);
-			   	return upper;
-   			}
+				static public function act_0(ground:String, pillar:String, ?upper:Int, ?lwhr:String){
+					temporas(upper, lwhr);
+					var command_n = [	[ 'install', 'hxcpp'],
+					[ 'config', 'http.postBuffer', '1524288000'],
+					[ '',  'init'],
+					[ 'clone', pillar, ground],
+					[ "--git-dir="+ground+"/.git", "--work-tree="+ground,  'checkout', 'master'],
+					[ "--git-dir="+ground+"/.git", "--work-tree="+ground,  'pull', 'origin', 'master'],
+					[ "--git-dir="+ground+"/.git", "--work-tree="+ground, "reset", "--hard"] ];
+					
+					if (!sys.FileSystem.exists( ground + "/.git" )) {
+						trace('Mark 1');
+						upper = clientele('git', command_n[3], upper);
+					} else {
+						upper = clientele('git', command_n[6], upper);
+					};
+					upper = clientele('git', command_n[4], upper);
+					upper = clientele('git', command_n[5], upper);
+					return upper;
+				}
 
-   			static public function act_1(bush:String, star:String, ?lower:Int, ?supr:String){
-   				temporas(lower, supr);
-   				var emcc = bush+'/emsdk.bat';
-   				var command_lx1 = ['clone', star, bush];
-   				var command_lx2 = ['clone', star, bush];
+				static public function act_1(bush:String, star:String, ?lower:Int, ?supr:String){
+					temporas(lower, supr);
+					var emcc = bush+'/emsdk.bat';
+					var command_lx1 = ['clone', star, bush];
+					var command_lx2 = ['clone', star, bush];
 
-   				var command_lx3 = ["--git-dir="+bush+"/.git", "--work-tree="+bush,  'checkout', 'master'];
-   				var command_lx4 = ["--git-dir="+bush+"/.git", "--work-tree="+bush,  'pull', 'origin', 'master'];
-   				var command_lx5 = ['update'];
-   				var command_lx6 = ['install', 'latest'];
-   				var command_lx7 = ['activate', 'latest'];
-   				var command_lx8 = [bush+'/emsdk_env.bat'];
+					var command_lx3 = ["--git-dir="+bush+"/.git", "--work-tree="+bush,  'checkout', 'master'];
+					var command_lx4 = ["--git-dir="+bush+"/.git", "--work-tree="+bush,  'pull', 'origin', 'master'];
+					var command_lx5 = ['update'];
+					var command_lx6 = ['install', 'latest'];
+					var command_lx7 = ['activate', 'latest'];
+					var command_lx8 = [bush+'/emsdk_env.bat'];
 
-   				trace(emcc);
-   				
-   				if (!sys.FileSystem.exists(bush)) {
-   					
-			   		lower = clientele('git', command_lx2, lower);
-   				};
-				lower = clientele('git', command_lx3, lower);
-			   	lower = clientele('git', command_lx4, lower);
-			   	lower = clientele( emcc, command_lx5, lower);
-			   	lower = clientele( emcc, command_lx6, lower);
-			   	lower = clientele( emcc, command_lx7, lower);
-			   	lower = clientele( 'source', command_lx8, lower);
+					trace(emcc);
+					
+					if (!sys.FileSystem.exists(bush)) {
+						
+						lower = clientele('git', command_lx2, lower);
+					};
+					lower = clientele('git', command_lx3, lower);
+					lower = clientele('git', command_lx4, lower);
+					lower = clientele( emcc, command_lx5, lower);
+					lower = clientele( emcc, command_lx6, lower);
+					lower = clientele( emcc, command_lx7, lower);
+					lower = clientele( 'source', command_lx8, lower);
 
-			   return lower;
-   			}
+					return lower;
+				}
 
-		   static public function act_2(mist:Int, dome:Int, ?meteo:Int, ?detsu:String){
-				temporas(meteo, detsu);
-				trace('Warning: Mark 3');
-		   		var command_m5 = ['checkout', '-b', 'feature-$dome'];
-			  	var command_m4a = ['add', '*'];
-			  	var command_m3 = ['commit', '-am', 'phrasing commith for the $mist -th time'];
-			  	var command_m2 = ['push', 'origin', 'feature-$dome'];
-			  	var command_m1 = ['checkout', 'development'];
-			  	var command_m0 = ['merge', 'feature-$dome'];
-				var command_0 = ['status'];
-			  	var command_1 = ['push', 'origin', 'development'];
-				var command_2 = ['checkout', 'master'];
-				var command_3 = ['merge', 'development'];
-				var command_4 = ['push', 'origin', 'master'];
-			  	clientele('git', command_m5);
-				clientele('git', command_m4a);
-			  	clientele('git', command_m3);
-			  	clientele('git', command_m2);
-			  	clientele('git', command_m1);
-			  	clientele('git', command_m0);
-				clientele('git', command_0);
-			  	clientele('git', command_1);
-			  	clientele('git', command_2);
-				clientele('git', command_0);
-			  	clientele('git', command_1);
-			  	clientele('git', command_2);
-			  	clientele('git', command_3);
-			  	clientele('git', command_4);
-			  	return meteo;
-			}
+				static public function act_2(mist:Int, dome:Int, ?meteo:Int, ?detsu:String){
+					temporas(meteo, detsu);
+					trace('Warning: Mark 3');
+					var command_m5 = ['checkout', '-b', 'feature-$dome'];
+					var command_m4a = ['add', '*'];
+					var command_m3 = ['commit', '-am', 'phrasing commith for the $mist -th time'];
+					var command_m2 = ['push', 'origin', 'feature-$dome'];
+					var command_m1 = ['checkout', 'development'];
+					var command_m0 = ['merge', 'feature-$dome'];
+					var command_0 = ['status'];
+					var command_1 = ['push', 'origin', 'development'];
+					var command_2 = ['checkout', 'master'];
+					var command_3 = ['merge', 'development'];
+					var command_4 = ['push', 'origin', 'master'];
+					clientele('git', command_m5);
+					clientele('git', command_m4a);
+					clientele('git', command_m3);
+					clientele('git', command_m2);
+					clientele('git', command_m1);
+					clientele('git', command_m0);
+					clientele('git', command_0);
+					clientele('git', command_1);
+					clientele('git', command_2);
+					clientele('git', command_0);
+					clientele('git', command_1);
+					clientele('git', command_2);
+					clientele('git', command_3);
+					clientele('git', command_4);
+					return meteo;
+				}
 
-			static public function gitcoal(jxmd:String, kxmd:String) {
-						if (!sys.FileSystem.exists(jxmd)){
-							sys.io.File.saveContent(jxmd, '1');
-						}
-						kxmd = sys.io.File.getContent(jxmd); 
-					  	var chr0n = Std.parseInt(kxmd);
-					  	if (kxmd != '' && chr0n != 0 ) {
-					  	chr0n++;
+				static public function gitcoal(jxmd:String, kxmd:String) {
+					if (!sys.FileSystem.exists(jxmd)){
+						sys.io.File.saveContent(jxmd, '1');
+					}
+					kxmd = sys.io.File.getContent(jxmd); 
+					var chr0n = Std.parseInt(kxmd);
+					if (kxmd != '' && chr0n != 0 ) {
+						chr0n++;
 						trace(chr0n);
 						trace('well $chr0n');
-					  	kxmd = Std.string(chr0n);
-					  	sys.io.File.saveContent(jxmd, kxmd);
-					 	} else {
-					  		chr0n = 1;
-					  		sys.io.File.append('1');
-					  	}
-					  	return chr0n;
+						kxmd = Std.string(chr0n);
+						sys.io.File.saveContent(jxmd, kxmd);
+					} else {
+						chr0n = 1;
+						sys.io.File.append('1');
+					}
+					return chr0n;
+				} 
+
+				static public function clientele(arxm:String, ?bptl:Array<String>, ?cyrr:Int) {
+					trace("Driving "+arxm+" @ "+bptl);
+					var process_4 = new sys.io.Process('$arxm', bptl);
+					trace("Warning: " + process_4.stdout.readAll().toString());
+					var commitHash_4 = process_4.stdout.readAll().toString();
+					if (process_4.exitCode() != 0) {
+						var message = process_4.stderr.readAll().toString();
+						var pos = haxe.macro.Context.currentPos();
+						haxe.macro.Context.error('Cannot execute process_$cyrr ... $process_4' + message, pos);
+					};
+					if ( cyrr > 0 ) cyrr++;
+					return cyrr;
+				}
+
+				static public function temporas(?mores:Int, ?oh:String) {
+					mores++;
+					var fame = DateTools.format(Date.now(), "Year::%Y::|::Month::%m::|::Day::%d::|::Hour::%H::|::Minute::%M::|::Second::%S::");
+					trace('Current::'+fame);
+					if ( sys.FileSystem.exists(oh) ){
+						var output = sys.io.File.append(oh, false);
+						output.writeString(fame+'\n');
+						output.close();
 					} 
+					return mores;
+				}
 
-					static public function clientele(arxm:String, ?bptl:Array<String>, ?cyrr:Int) {
-							trace("Driving "+arxm+" @ "+bptl);
-							var process_4 = new sys.io.Process('$arxm', bptl);
-							trace("Warning: " + process_4.stdout.readAll().toString());
-							var commitHash_4 = process_4.stdout.readAll().toString();
-							if (process_4.exitCode() != 0) {
-							var message = process_4.stderr.readAll().toString();
-							var pos = haxe.macro.Context.currentPos();
-							haxe.macro.Context.error('Cannot execute process_$cyrr ... $process_4' + message, pos);
+				static public function mores(ether:String, ?est:String){
+					if (sys.FileSystem.exists(ether)){
+						var inter = sys.io.File.getContent(ether); 
+						if (sys.FileSystem.exists(est)){
+							var syg:String = sys.io.File.getContent(est);
+							var sygm:Array<String> = syg.split('\n');
+							var hop:Int = 2;
+							var ry3g:String = "\\b(";
+							var ry4g:String =  ")\\b";
+							var ry1g:String = ry3g;
+							for (y in sygm ) { 
+								ry1g = ry1g + y + "|";
 							};
-							if ( cyrr > 0 ) cyrr++;
-							return cyrr;
-					}
-	
-					static public function temporas(?mores:Int, ?oh:String) {
-						mores++;
-						var fame = DateTools.format(Date.now(), "Year::%Y::|::Month::%m::|::Day::%d::|::Hour::%H::|::Minute::%M::|::Second::%S::");
-						trace('Current::'+fame);
-						if ( sys.FileSystem.exists(oh) ){
-							var output = sys.io.File.append(oh, false);
-							  output.writeString(fame+'\n');
-							  output.close();
-					 	} 
-					  	return mores;
-					}
-
-					static public function mores(ether:String, ?est:String){
-						if (sys.FileSystem.exists(ether)){
-							var inter = sys.io.File.getContent(ether); 
-							if (sys.FileSystem.exists(est)){
-								var syg:String = sys.io.File.getContent(est);
-								var sygm:Array<String> = syg.split('\n');
-								var hop:Int = 2;
-								var ry3g:String = "\\b(";
-								var ry4g:String =  ")\\b";
-								var ry1g:String = ry3g;
-								for (y in sygm ) { 
-									ry1g = ry1g + y + "|";
-								};
-								ry1g = ry1g + ry4g;
-								trace('Errror $ry1g');
-							}
-							
-							var undone = false, currun, curran;
-							while ( undone == false ){
-								curran = Std.int(Math.min(inter.indexOf(' '), inter.indexOf('\n')));
-								if ( curran >= 0 ) {
-									currun = inter.substr(0, curran);
-									//trace("Error " + curran + " " + currun.length);
-									inter = inter.substr(currun.length + 1);
-									//trace("Warning: " + currun);
-								} else {
-									undone = true;
-								}
-							}
+							ry1g = ry1g + ry4g;
+							trace('Errror $ry1g');
 						}
-					}
+						
+						var undone = false, currun, curran;
+						while ( undone == false ){
+							curran = Std.int(Math.min(inter.indexOf(' '), inter.indexOf('\n')));
+							if ( curran >= 0 ) {
+								currun = inter.substr(0, curran);
+													//trace("Error " + curran + " " + currun.length);
+													inter = inter.substr(currun.length + 1);
+													//trace("Warning: " + currun);
+												} else {
+													undone = true;
+												}
+											}
+										}
+									}
 
 
-}
+								}
 
-class Postulatum {
+								class Postulatum {
 
-}
+								}
